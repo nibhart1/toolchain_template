10f81d2244465c3199b751b45171babc9e2a5ce8
nibhart1 <57429142+nibhart1@users.noreply.github.com>
2020-06-23 10:20:03 +0530

    Update main.tf

--- ibm-cos-bucket/main.tf
+++ ibm-cos-bucket/main.tf
@@ -1,5 +1,5 @@
 data "ibm_resource_group" "cos_group" {
-  name = var.resource_group_name
+  name = "prod"
 }
 resource "ibm_resource_instance" "cos_instance" {
   name              = "cos-instance"
