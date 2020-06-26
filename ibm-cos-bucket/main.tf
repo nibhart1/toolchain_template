2108a35038388d84d16fa224bd93740c79ef4b36
nibhart1 <57429142+nibhart1@users.noreply.github.com>
2020-06-26 18:14:25 +0530

    Update main.tf

--- ibm-cos-bucket/main.tf
+++ ibm-cos-bucket/main.tf
@@ -1,10 +1,10 @@
 
 resource "ibm_resource_group" "cos_group" {
-  name     = "prod2"
+  name     = "resource-prod"
 }
 
 resource "ibm_resource_instance" "cos_instance" {
-  name              = "my-cos-instance"
+  name              = "my-cos-instance1"
   resource_group_id = ibm_resource_group.cos_group.id
   service           = "cloud-object-storage"
   plan              = "standard"
@@ -12,7 +12,7 @@ resource "ibm_resource_instance" "cos_instance" {
 }
 
 resource "ibm_cos_bucket" "standard-ams03" {
-  bucket_name          = "ibm-my-bucket"
+  bucket_name          = "ibm-my-bucket1"
   resource_instance_id = ibm_resource_instance.cos_instance.id
   region_location      = "us-south"
   storage_class        = "standard"
