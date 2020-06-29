565975f4893a7f38222b41804fbc5638f55c1f21
nibhart1 <57429142+nibhart1@users.noreply.github.com>
2020-06-29 17:57:38 +0530

    Update provider.tf

--- ibm-cos-bucket/provider.tf
+++ ibm-cos-bucket/provider.tf
@@ -1,10 +1,3 @@
-variable "iaas_classic_username" {
-  description = "Enter the user name to access IBM Cloud classic infrastructure. You can retrieve the user name by following the instructions for retrieving your classic infrastructure API key."
-}
-
-variable "iaas_classic_api_key" {
-  description = "Enter the API key to access IBM Cloud classic infrastructure. For more information for how to create an API key and retrieve it, see [Managing classic infrastructure API keys](https://cloud.ibm.com/docs/iam?topic=iam-classic_keys)."
-}
 
 variable "ibmcloud_api_key" {
   description = "Enter your IBM Cloud API Key, you can get your IBM Cloud API key using: https://cloud.ibm.com/iam#/apikeys"
